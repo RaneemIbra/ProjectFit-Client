c1d9728ce2c390e5332a5b2789ef82c3853ad790
RaneemIbra <132738285+RaneemIbra@users.noreply.github.com>
2024-08-21 22:51:11 +0300

    testing the pipeline2

--- app/src/main/java/com/example/projectfit/SecondaryActivity.java
+++ app/src/main/java/com/example/projectfit/SecondaryActivity.java
@@ -1,6 +1,5 @@
 package com.example.projectfit;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -10,23 +9,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    Button nav;
+public class SecondaryActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_secondary);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        nav = findViewById(R.id.button);
-        nav.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, SecondaryActivity.class);
-            startActivity(intent);
-        });
+        btn = findViewById(R.id.button2);
+        btn.setOnClickListener(view -> finish());
     }
 }

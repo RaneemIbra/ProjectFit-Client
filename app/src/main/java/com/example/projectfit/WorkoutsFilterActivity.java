69ec72797bea93f3004346ab4a8207d1c0704c57
aseel <aseel.nahhas84@gmail.com>
2024-08-29 16:17:36 +0300

    workoutFilter 90% done

--- app/src/main/java/com/example/projectfit/WorkoutsFilterActivity.java
+++ app/src/main/java/com/example/projectfit/WorkoutsFilterActivity.java
@@ -1,12 +1,15 @@
 package com.example.projectfit;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.bumptech.glide.Glide;
+import com.google.android.material.imageview.ShapeableImageView;
 
 public class WorkoutsFilterActivity extends AppCompatActivity {
 
@@ -20,5 +23,14 @@ public class WorkoutsFilterActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ShapeableImageView photo=findViewById(R.id.legs_image);
+        photo.setImageResource(R.drawable.img);
+
+       // Glide.with(this).load("drawable/img.png").into((ShapeableImageView)findViewById(R.id.r4en2wcm8cu));
+        //Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into((ShapeableImageView)findViewById(R.id.rj2eqf4csatn));
+        //Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into((ShapeableImageView)findViewById(R.id.rf7hmijhd478));
+        //Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into((ShapeableImageView)findViewById(R.id.rmkb1m5uwabg));
+        //Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into((ShapeableImageView)findViewById(R.id.rrj38y1qpc3));
     }
 }

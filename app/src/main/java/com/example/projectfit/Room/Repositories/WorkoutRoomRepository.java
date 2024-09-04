d5c7328f6da24a725e470f8ba65b4bb234a40960
RaneemIbra <132738285+RaneemIbra@users.noreply.github.com>
2024-09-04 15:58:25 +0300

    worked on the backend for the workouts

--- app/src/main/java/com/example/projectfit/Room/Repositories/WorkoutRoomRepository.java
+++ app/src/main/java/com/example/projectfit/Room/Repositories/WorkoutRoomRepository.java
@@ -1,4 +1,23 @@
 package com.example.projectfit.Room.Repositories;
 
+import android.content.Context;
+
+import com.example.projectfit.Models.Workout;
+import com.example.projectfit.Room.Databases.WorkoutDatabase;
+
+import java.util.concurrent.Executors;
+
 public class WorkoutRoomRepository {
+    private final WorkoutDatabase workoutDatabase;
+
+    public WorkoutRoomRepository(Context context) {
+        workoutDatabase = WorkoutDatabase.getInstance(context.getApplicationContext());
+    }
+
+    public void addWorkoutLocally(Workout workout) {
+        Executors.newSingleThreadExecutor().execute(() -> {
+            workoutDatabase.workoutDAO().insertWorkout(workout);
+            System.out.println("is it working?");
+        });
+    }
 }

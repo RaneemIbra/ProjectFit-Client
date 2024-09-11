8301c713a5ff88c9368e6028c6094979fc66c6ee
RaneemIbra <132738285+RaneemIbra@users.noreply.github.com>
2024-09-11 22:23:42 +0300

    minor issues with the pages were fixed and worked on the loading screen

--- app/src/main/java/com/example/projectfit/Activities/LoadingScreenActivity.java
+++ app/src/main/java/com/example/projectfit/Activities/LoadingScreenActivity.java
@@ -1,6 +1,9 @@
 package com.example.projectfit.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
 import android.widget.ImageView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +16,8 @@ import com.example.projectfit.R;
 public class LoadingScreenActivity extends AppCompatActivity {
 
     private ImageView gifImageView;
+    Button registerBTN, loginBTN;
+    private static final int DELAY_MILLIS = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +30,34 @@ public class LoadingScreenActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        gifImageView = findViewById(R.id.gifImageView);
+        initViews();
+        setUpClickListeners();
         String gifUrl = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExODFocThmamZ0cGJ5Zm82aXJvYmQ0cWR2cXR1enJzOGM2YXZ4MnVwMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/0BtrpaGo15NgvitihA/giphy.gif";
         Glide.with(this)
                 .asGif()
                 .load(gifUrl)
                 .into(gifImageView);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(LoadingScreenActivity.this, RegisterActivity.class); // Change MainActivity to your target activity
+            startActivity(intent);
+            finish();
+        }, DELAY_MILLIS);
+    }
+
+    private void initViews(){
+        gifImageView = findViewById(R.id.gifImageView);
+        registerBTN = findViewById(R.id.registerButtonLoadingScreen);
+        loginBTN = findViewById(R.id.LoginButtonLoadingScreen);
+    }
+    private void setUpClickListeners(){
+        registerBTN.setOnClickListener(v -> {
+            Intent intent = new Intent(LoadingScreenActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
+        loginBTN.setOnClickListener(v -> {
+            Intent intent = new Intent(LoadingScreenActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
